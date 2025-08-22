722b305417334c1bde635a2d4dcf2a18a9ecf9b0
Zherekhov Serhii <96929793+serhii-zherekhov@users.noreply.github.com>
2025-08-22 22:29:37 +0300

    Cleansing_Data

---
+++ scripts/12_CheckingData.sql
@@ -0,0 +1,29 @@
+SELECT 
+	*
+FROM silver.crm_cust_info
+GO
+
+SELECT 
+	*
+FROM silver.crm_prd_info
+GO
+
+SELECT 
+	*
+FROM silver.crm_sales_details
+GO
+
+SELECT 
+	*
+FROM silver.erp_cust_az12
+GO
+
+SELECT 
+	*
+FROM silver.erp_loc_a101
+GO
+
+SELECT 
+	*
+FROM silver.erp_px_cat_g1v2
+GO
